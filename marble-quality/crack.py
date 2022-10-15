2e96f6c1e22b181a2c1c359420745f2026f89cd3
ilker yilmaz <ilkery549@gmail.com>
2022-10-15 12:30:25 +0300

    added color and crack detection

---
+++ marble-quality/crack.py
@@ -0,0 +1,75 @@
+import cv2
+import math
+import numpy as np
+import scipy.ndimage
+
+
+def orientated_non_max_suppression(mag, ang):
+    ang_quant = np.round(ang / (np.pi / 4)) % 4
+    winE = np.array([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+    winSE = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    winS = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+    winSW = np.array([[0, 0, 1], [0, 1, 0], [1, 0, 0]])
+
+    magE = non_max_suppression(mag, winE)
+    magSE = non_max_suppression(mag, winSE)
+    magS = non_max_suppression(mag, winS)
+    magSW = non_max_suppression(mag, winSW)
+
+    mag[ang_quant == 0] = magE[ang_quant == 0]
+    mag[ang_quant == 1] = magSE[ang_quant == 1]
+    mag[ang_quant == 2] = magS[ang_quant == 2]
+    mag[ang_quant == 3] = magSW[ang_quant == 3]
+    return mag
+
+
+def non_max_suppression(data, win):
+    data_max = scipy.ndimage.filters.maximum_filter(data, footprint=win, mode='constant')
+    data_max[data != data_max] = 0
+    return data_max
+
+
+# start calulcation
+gray_image = cv2.imread('mermer-elazig-visne.jpg', 0)
+
+with_nmsup = True  # apply non-maximal suppression
+fudgefactor = 1.3  # with this threshold you can play a little bit
+sigma = 21  # for Gaussian Kernel
+kernel = 2 * math.ceil(2 * sigma) + 1  # Kernel size
+
+gray_image = gray_image / 255.0
+blur = cv2.GaussianBlur(gray_image, (kernel, kernel), sigma)
+gray_image = cv2.subtract(gray_image, blur)
+
+# compute sobel response
+sobelx = cv2.Sobel(gray_image, cv2.CV_64F, 1, 0, ksize=3)
+sobely = cv2.Sobel(gray_image, cv2.CV_64F, 0, 1, ksize=3)
+mag = np.hypot(sobelx, sobely)
+ang = np.arctan2(sobely, sobelx)
+
+# threshold
+threshold = 4 * fudgefactor * np.mean(mag)
+mag[mag < threshold] = 0
+
+# either get edges directly
+if with_nmsup is False:
+    mag = cv2.normalize(mag, 0, 255, cv2.NORM_MINMAX)
+    kernel = np.ones((5, 5), np.uint8)
+    result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow('im', result)
+    cv2.waitKey()
+
+# or apply a non-maximal suppression
+else:
+
+    # non-maximal suppression
+    mag = orientated_non_max_suppression(mag, ang)
+    # create mask
+    mag[mag > 0] = 255
+    mag = mag.astype(np.uint8)
+
+    kernel = np.ones((5, 5), np.uint8)
+    result = cv2.morphologyEx(mag, cv2.MORPH_CLOSE, kernel)
+
+    cv2.imshow('im', result)
+    cv2.waitKey()
---
+++ marble-quality/renk-tespiti.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rnd
+import statistics
+
+img_path = "mermer-elazig-visne.jpg"
+
+img = cv2.imread(img_path)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+randomize = img.shape[0] * img.shape[1]
+print(int(randomize * 0.08))
+renk = []
+# resim yüzde 8 i kadar random piksel seçiyoruz
+
+for i in range(int(randomize * 0.08)):
+    x = rnd.randint(0, img.shape[0] - 1)
+    y = rnd.randint(0, img.shape[1] - 1)
+    renk.append(img[x][y])
+
+# Seçili random piksellerin r g b renk kodlarını başka diziye aktarıyoruz
+r = []
+g = []
+b = []
+# Ortalama bir renk kodu elde etmek için ayrıştıracağımız [r,g,b] kodlarının ortalamasını alarak renk elde ediyoruz
+for a in range(len(renk)):
+    r.append(renk[a][0])
+    g.append(renk[a][1])
+    b.append(renk[a][2])
+# Elde edilen listenin ortalaması alınıyor
+print("Örneğimizin rgb kodu : ", int(statistics.mean(r)), int(statistics.mean(g)), int(statistics.mean(b)))
+
+# Renk kodlarını kullanarak renk ismini buluyoruz
+import pandas as pd
+
+index = ["color", "color_name", "hex", "R", "G", "B"]
+csv = pd.read_csv('colors.csv', names=index, header=None)
+
+
+def getColorName(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+print("Örneğimizin renk ismi : ",
+      getColorName(int(statistics.mean(r)), int(statistics.mean(g)), int(statistics.mean(b))))
+
+plt.imshow(img)
+plt.show()
