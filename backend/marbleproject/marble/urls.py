db0684d4a40356aab573513f7c056de0dedfe3da
İlker YILMAZ <53461108+ilker-yilmaz@users.noreply.github.com>
2022-12-22 11:07:37 +0300

    tur-tespiti

--- backend/marbleproject/marble/urls.py
+++ backend/marbleproject/marble/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.anasayfa, name="anasayfa")
+    path("", views.anasayfa, name="anasayfa"),
+    # path('upload/', views.upload_image, name='upload'),
+    # path('result/', views.upload_image, name='result'),
+
 ]
